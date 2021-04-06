b8f923111b42197c41927ad8d767135355a0bab5
alusoso <2530056295@qq.com>
2021-04-06 10:30:22 +0800

    添加方法

--- scripts/test_jenkins.py
+++ scripts/test_jenkins.py
@@ -5,3 +5,14 @@ class TestJenkins:
 
     def test_login2(self):
         print("login2")
+
+    def test_login3(self):
+        print("login3")
+        assert False
+
+    def test_login4(self):
+        print("login4")
+
+    def test_login5(self):
+        print("login5")
+        assert False
