cb3c19c7b6d7aeafbe3f5f82d2847ec6254bea70
alusoso <2530056295@qq.com>
2021-04-06 09:56:53 +0800

    first

---
+++ scripts/test_jenkins.py
@@ -0,0 +1,11 @@
+class TestJenkins:
+
+    def test_login1(self):
+        print("login1")
+
+    def test_login2(self):
+        print("login2")
+
+    def test_login3(self):
+        print("login3")
+        assert False
